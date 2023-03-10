acb1a266d35f20efdd9b262c2fd138988bea23ae
LuanTenorio <lluantenorio7@gmail.com>
2023-03-10 16:43:55 -0300

    add informations

---
+++ src/app/informations/informations.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InformationsComponent } from './informations/informations.component';
+
+
+
+@NgModule({
+  declarations: [
+    InformationsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InformationsModule { }
