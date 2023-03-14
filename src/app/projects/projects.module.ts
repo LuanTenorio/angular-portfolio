566a76d9b791c961c4e842968c0f79d7d0ea33d7
LuanTenorio <lluantenorio7@gmail.com>
2023-03-13 21:28:30 -0300

    patch folders of projects

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './project/project.component';
+import { AddProjectComponent } from './add-project/add-project.component';
 
 @NgModule({
   declarations: [
-    ProjectsComponent
+    ProjectsComponent,
+    ProjectComponent,
+    AddProjectComponent
   ],
   imports: [
     CommonModule
